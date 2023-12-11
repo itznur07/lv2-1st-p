84b9844f880ab56ea336b718ba5e771bbe3c6f55
Md Nuruddin <nuruddinalways99@gmail.com>
2023-12-11 08:44:58 +0600

    error handling give custom statusCode

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import AppError from '../../errors/AppError';
 import { TAcademicDepartment } from './academicDepartment.interface';
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>({
@@ -11,8 +12,31 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>({
   academicFaculty: {
     type: Schema.Types.ObjectId,
     ref: 'academicFacultyModel',
-    required: true,
   },
+});
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isExistsDepartment = await academicDepartmentModel.findOne({
+    name: this.name,
+  });
+
+  if (isExistsDepartment) {
+    throw new Error('This academic department is already exists!');
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExists = await academicDepartmentModel.findOne(query);
+
+  if (!isDepartmentExists) {
+    throw new AppError(404, 'department is does not exits');
+  }
+
+  next();
 });
 
 export const academicDepartmentModel = model<TAcademicDepartment>(
--- src/app/modules/academicFaculty/academicFacultyModel.ts
+++ src/app/modules/academicFaculty/academicFacultyModel.ts
@@ -14,6 +14,30 @@ const academicFacultySchema = new Schema<TAcademciFaculty>(
   },
 );
 
+academicFacultySchema.pre('save', async function (next) {
+  const isExistsFaculty = await academicFacultyModel.findOne({
+    name: this.name,
+  });
+
+  if (isExistsFaculty) {
+    throw new Error('This academic faculty is already exists!');
+  }
+
+  next();
+});
+
+academicFacultySchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isExistsFaculty = await academicFacultyModel.findOne(query);
+
+  if (!isExistsFaculty) {
+    throw new Error('This academic faculty does not exists!');
+  }
+
+  next();
+});
+
 export const academicFacultyModel = model<TAcademciFaculty>(
   'AcademicFaculty',
   academicFacultySchema,
